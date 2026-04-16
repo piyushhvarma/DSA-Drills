d56130d91f0d32270a69d7ea43ee774036328b9e
Piyush Varma <pytworks@gmail.com>
2026-04-16 18:16:31 +0530

    Add selection sort implementation and largest element function

---
+++ Cpp/Revision/Selection-Sort.cpp
@@ -0,0 +1,4 @@
+// 1. we will need 2 loops, 1st i loop will traverse the array to n-1 and will consider the initial position "mini" as the minimum value
+// 2. the second loop will iterate the array and will find out if there is any number smaller than the mini, if there exists a number we swap it using temp
+// 3. swap the numbers arr[j], arr[mini], arr[i]
+// write int main function
